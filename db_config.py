72df2da428bc904f9548727e9eb5c48905e99592
Marcello Dichiera <marcello_dichiera@hotmail.it>
2024-11-21 01:13:05 +0000

    quick fix

--- db_config.py
+++ db_config.py
@@ -5,9 +5,9 @@ import os
 CONNECTION_STRING = (
     
     f"host={os.getenv('DATABASE_HOST')}"
-    f" port={os.getenv('DATABASE_NAME')}"
-    f" dbname={os.getenv('DATABASE_USER')}"
-    f" user={os.getenv('DATABASE_PASSWORD')}"
-    f" password={os.getenv('DATABASE_PORT')}"
+    f" name={os.getenv('DATABASE_NAME')}"
+    f" user={os.getenv('DATABASE_USER')}"
+    f" password={os.getenv('DATABASE_PASSWORD')}"
+    f" port={os.getenv('DATABASE_PORT')}"
 
 )
