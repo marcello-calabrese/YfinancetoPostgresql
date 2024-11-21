23ce434fc3f7520f251e5b9d2698b91f1aa61e8e
Marcello Dichiera <marcello_dichiera@hotmail.it>
2024-11-21 00:43:29 +0000

    preparing file for github actions

--- db_config.py
+++ db_config.py
@@ -1,3 +1,13 @@
 ## connection string to postgresql database 
 
-CONNECTION_STRING = "host='localhost' dbname='Personal_Shares' user='postgres' password='Lorenzo_22' port='5432'"
+import os
+
+CONNECTION_STRING = (
+    
+    f"host={os.getenv('DATABASE_HOST')}"
+    f" port={os.getenv('DATABASE_NAME')}"
+    f" dbname={os.getenv('DATABASE_USER')}"
+    f" user={os.getenv('DATABASE_PASSWORD')}"
+    f" password={os.getenv('DATABASE_PORT')}"
+
+)
