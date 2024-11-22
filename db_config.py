81625a1dc685d89777f0aed13d06761e6a821c5e
Marcello Dichiera <marcello_dichiera@hotmail.it>
2024-11-22 11:08:06 +0000

    seconf fix

--- db_config.py
+++ db_config.py
@@ -4,10 +4,10 @@ import os
 
 CONNECTION_STRING = (
     
-    f" host={os.getenv('DATABASE_HOST')}"
-    f" name={os.getenv('DATABASE_NAME')}"
-    f" user={os.getenv('DATABASE_USER')}"
-    f" password={os.getenv('DATABASE_PASSWORD')}"
-    f" port={os.getenv('DATABASE_PORT')}"
+    f" Host={os.getenv('DATABASE_HOST')}"
+    f" Database={os.getenv('DATABASE_NAME')}"
+    f" Username={os.getenv('DATABASE_USER')}"
+    f" Password={os.getenv('DATABASE_PASSWORD')}"
+    f" Port={os.getenv('DATABASE_PORT')}"
 
 )
